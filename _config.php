52c3315d4cc4593cf29647b09e2ece208c968d0b
Colin Tucker <colin@praxis.net.au>
2017-11-02 15:03:01 +1100

    Prepared for release

---
+++ _config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * {module-name} configuration file.
+ *
+ * PHP version >=5.6.0
+ *
+ * For full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package {namespace}
+ * @author {author} <{email}>
+ * @copyright {year} {organisation}
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @link {repo-url}
+ */
